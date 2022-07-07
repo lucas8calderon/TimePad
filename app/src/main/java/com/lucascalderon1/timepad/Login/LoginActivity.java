cbf582f274dec2d20efd3762923386ae414b0455
Lucas Calderon <lucas8calderon@hotmail.com>
2022-07-07 15:35:20 -0300

    autenticacao de senha

--- app/src/main/java/com/lucascalderon1/timepad/Login/LoginActivity.java
+++ app/src/main/java/com/lucascalderon1/timepad/Login/LoginActivity.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.lucascalderon1.timepad.MainActivity;
 import com.lucascalderon1.timepad.R;
-
-
+import com.lucascalderon1.timepad.helper.FirebaseHelper;
 
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputEditText  etEmail, loginSenha;
+    private ProgressBar progressBar;
 
 
 
@@ -21,15 +27,13 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        etEmail = findViewById(R.id.etEmail);
+        loginSenha = findViewById(R.id.loginSenha);
+        progressBar = findViewById(R.id.progressBar);
+
         findViewById(R.id.txt_criar).setOnClickListener(v -> {
             finish();
             startActivity(new Intent(this, RegistroActivity.class));
-        });
-
-
-        findViewById(R.id.btn_avancar).setOnClickListener(v -> {
-            finish();
-            startActivity(new Intent(this, MainActivity.class));
         });
 
         findViewById(R.id.txt_recuperar).setOnClickListener(v -> {
@@ -37,6 +41,47 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(new Intent(this, RecuperaActivity.class));
         });
 
+
+
+
+    }
+
+    public void validaDados(View view) {
+        String email = etEmail.getText().toString();
+        String senha = loginSenha.getText().toString();
+
+        if (!email.isEmpty()){
+            if (!senha.isEmpty()){
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                logar(email, senha);
+
+
+            } else  {
+                loginSenha.requestFocus();
+                loginSenha.setError("Informe sua senha");
+            }
+
+        } else  {
+            etEmail.requestFocus();
+            etEmail.setError("Informe seu e-mail");
+        }
+
+    }
+
+    private void logar(String email, String senha) {
+        FirebaseHelper.getAuth().signInWithEmailAndPassword(
+                email, senha
+        ).addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                finish();
+                startActivity(new Intent(this, MainActivity.class));
+            } else {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(this, "Senha ou e-mail incorretos", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
